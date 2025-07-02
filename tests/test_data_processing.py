d5c62adbf9a4772a608dfede31e45ced5ed395ee
Hiwot Teshome <lhiwot181@gmail.com>
2025-07-02 03:15:28 +0300

    chore: mark test imports with noqa to satisfy flake8

--- tests/test_data_processing.py
+++ tests/test_data_processing.py
@@ -1,5 +1,5 @@
-import pandas as pd
-from src.data_processing import (
+import pandas as pd  # noqa: F401
+from src.data_processing import (  # noqa: F401
     RFMTransformer,
     TemporalAggregator,
     CategoricalAggregator,
