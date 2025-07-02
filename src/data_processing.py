e268c81a438051fae4c11135cbd0fb9d22d7518d
Hiwot Teshome <lhiwot181@gmail.com>
2025-07-02 03:07:57 +0300

    chore: remove unused imports to satisfy flake8

--- src/data_processing.py
+++ src/data_processing.py
@@ -2,8 +2,7 @@ import argparse
 import pandas as pd
 
 from sklearn.base import BaseEstimator, TransformerMixin
-from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.preprocessing import StandardScaler
 
 
 class RFMTransformer(BaseEstimator, TransformerMixin):
